e2aa2a3322aae8bfd11afd34baba5bdeabd5f969
Ryan McCahon <mccahonr@yahoo.com>
2019-08-22 14:29:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,4 +44,11 @@ class Song
     @@all.sort_by{|x| x.name}
   end
 
+  def self.new_from_filename
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+
 end
