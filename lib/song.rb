992b82a6a395681e979989d66e1cb7751a21acb3
Ryan McCahon <mccahonr@yahoo.com>
2019-08-22 13:45:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,6 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
   def self.all
     @@all
   end
@@ -15,9 +10,8 @@ class Song
     self.class.all << self
   end
 
-  def self.create(name)
+  def self.create
     song = self.new
-    song.name = name
     @@all << song
   end
 
